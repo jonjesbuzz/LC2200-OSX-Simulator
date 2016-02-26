06f6f5283f8a7bbaaf1ecb5c2bf72d722ef48ed8
Jonathan Jemson <jonathanjemson@gatech.edu>
2016-02-26 00:31:51 -0500

    Initial commit

---
+++ LC-2200 Simulator/LC2200DataSource.swift
@@ -0,0 +1,82 @@
+//
+//  LC2200DataSource.swift
+//  LC-2200 Simulator
+//
+//  Created by Jonathan Jemson on 2/25/16.
+//  Copyright © 2016 Jonathan Jemson. All rights reserved.
+//
+
+import Cocoa
+
+extension ViewController: NSTableViewDataSource {
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        switch tableView {
+        case self.memoryTableView:
+            return self.processor.memory.count
+        case self.registerTableView:
+            return self.processor.registers.registers.count
+        default:
+            return 0
+        }
+    }
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    func tableView(tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return (tableView != memoryTableView)
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if tableView == self.memoryTableView {
+            return configureMemoryTableCell(column: tableColumn, row: row)
+        } else if tableView == self.registerTableView {
+            return configureRegisterTableCell(column: tableColumn, row: row)
+        }
+        return nil
+    }
+    
+    private func configureMemoryTableCell(column column: NSTableColumn?, row: Int) -> NSView? {
+        var text = ""
+        var identifier = ""
+        let memLoc = processor.memory[row]
+        if column == memoryTableView.tableColumns[0] {
+            text = String(format: "0x%04x", row)
+            identifier = "addressCellID"
+        } else if column == memoryTableView.tableColumns[1] {
+            text = "\(memLoc)"
+            identifier = "decimalCellID"
+        } else if column == memoryTableView.tableColumns[2] {
+            text = String(format: "0x%04x", memLoc)
+            identifier = "hexCellID"
+        } else if column == memoryTableView.tableColumns[3] {
+            text = Instruction(value: memLoc).debugDescription
+            identifier = "instructionCellID"
+        }
+        if let cell = self.memoryTableView.makeViewWithIdentifier(identifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
+    
+    private func configureRegisterTableCell(column column: NSTableColumn?, row: Int) -> NSView? {
+        var text = ""
+        var identifier = ""
+        if column == registerTableView.tableColumns[0] {
+            text = RegisterFile.Register(rawValue: UInt8(truncatingBitPattern: row))!.description
+            identifier = "registerCellID"
+        } else if column == registerTableView.tableColumns[1] {
+            text = "\(processor.registers.registers[row])"
+            if (row >= 13) {
+                text = String(format: "0x%04x", processor.registers.registers[row])
+            }
+            identifier = "registerValueCellID"
+        }
+        if let cell = self.registerTableView.makeViewWithIdentifier(identifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
+}
--- LC-2200 Simulator/ViewController.swift
+++ LC-2200 Simulator/ViewController.swift
@@ -9,11 +9,91 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    internal var processor: LC2200Processor!
 
+    @IBOutlet weak var memoryTableView: NSTableView!
+    @IBOutlet weak var registerTableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.processor = LC2200Processor()
+        memoryTableView.setDelegate(self)
+        registerTableView.setDelegate(self)
+        memoryTableView.setDataSource(self)
+        registerTableView.setDataSource(self)
+        registerTableView.target = self
+        registerTableView.doubleAction = #selector(ViewController.goToRegister)
+    }
+    
+    func goToRegister() {
+        let register = RegisterFile.Register(rawValue: UInt8(truncatingBitPattern: registerTableView.selectedRow))!
+        let index = Int(processor.registers[register])
+        self.memoryTableView.scrollRowToVisible(index)
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["lc"]
+        let result = openPanel.runModal()
+        openPanel.allowsMultipleSelection = false
+        openPanel.allowsOtherFileTypes = false
+        openPanel.canChooseDirectories = false
+        openPanel.canChooseFiles = true
+        if result == NSFileHandlingPanelOKButton {
+            let memory: String
+            do {
+                memory = try String(contentsOfURL: openPanel.URLs.first!, encoding: NSUTF8StringEncoding)
+            } catch {
+                print(error)
+                exit(1)
+            }
+            let vals = memory.characters.split { $0 == " " || $0 == "\n" }.map(String.init)
+            let things = vals.map { (s) -> (UInt16) in
+                if let data = UInt16(s, radix: 16) {
+                    return data
+                }
+                print("File \(openPanel.URLs.first!) is not a valid LC2200 file.")
+                exit(1)
+            }
+            processor.setupMemory(things)
+            self.memoryTableView.reloadData()
+            let indexSet = NSIndexSet(index: Int(self.processor.currentAddress))
+            self.memoryTableView.selectRowIndexes(indexSet, byExtendingSelection: false)
+            self.memoryTableView.allowsEmptySelection = false
+            self.registerTableView.reloadData()
+        } else {
+            exit(0)
+        }
+    }
+    @IBAction func runProcessor(sender: NSButton) {
+        processor.run()
+        processorDidChange()
+    }
+    
+    @IBAction func stepProcessor(sender: NSButton) {
+        processor.step()
+        processorDidChange()
+    }
+    
+    @IBAction func rewindProcessor(sender: NSButton) {
+        processor.rewind()
+        processorDidChange()
+    }
+    
+    @IBAction func resetProcessor(sender: NSButton) {
+        processor.reset()
+        processorDidChange()
+    }
+    
+    private func processorDidChange() {
+        let indexSet = NSIndexSet(index: Int(self.processor.currentAddress))
+        self.memoryTableView.reloadData()
+        self.memoryTableView.selectRowIndexes(indexSet, byExtendingSelection: false)
+        self.memoryTableView.scrollRowToVisible(indexSet.firstIndex)
+        self.registerTableView.reloadData()
     }
 
     override var representedObject: AnyObject? {
